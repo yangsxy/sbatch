3c8cf9fd48bedff36e1b25318969f9bdbd8bac1a
yuandg <yuandg@shishike.com>
2016-07-13 03:55:41 +0800

    jsp

--- src/main/java/cn/yuan/Application.java
+++ src/main/java/cn/yuan/Application.java
@@ -1,17 +1,27 @@
 package cn.yuan;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration
+@EnableAutoConfiguration
+@Controller
 @SpringBootApplication//spring boot入口必须配
 @ServletComponentScan//必须配此，filter、listener才有用
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+	@RequestMapping("/index")
+	public String home(){
+		System.out.println("来啦");
+		return "home.jsp";
+	}
 }
