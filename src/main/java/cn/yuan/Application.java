b4a7696ac4f3d9ef98f06ab1cfdca77bd80578ec
yuandg <yuandg@shishike.com>
2016-07-19 18:17:44 +0800

    添加jsp支持

--- src/main/java/cn/yuan/Application.java
+++ src/main/java/cn/yuan/Application.java
@@ -1,18 +1,15 @@
 package cn.yuan;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@Configuration
-@EnableAutoConfiguration
 @Controller
-@SpringBootApplication//spring boot入口必须配
+@SpringBootApplication//该 @SpringBootApplication 注解等价于以默认属性使用 @Configuration ， @EnableAutoConfiguration 和 @ComponentScan 。
 @ServletComponentScan//必须配此，filter、listener才有用
 public class Application extends SpringBootServletInitializer {
 
@@ -20,8 +17,10 @@ public class Application extends SpringBootServletInitializer {
 		SpringApplication.run(Application.class, args);
 	}
 	@RequestMapping("/index")
-	public String home(){
-		System.out.println("来啦");
-		return "home.jsp";
+	public ModelAndView home(){
+		
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("hi", "hhahhh");
+		return mv;
 	}
 }
